76f2528cd235ef23750771b1e8dbb8449746f3e3
quang-hung.nguyen <quang-hung.nguyen@quant-edge.com>
2021-12-02 12:10:03 +0700

    end day1 - lab1

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,61 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class _RandomWordsState123 extends State<RandomWords> {
-  @override                                  
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();      // NEW
-    return Text(wordPair.asPascalCase);      // NEW
-  }                                         
-}
 
 class RandomWords extends StatefulWidget {
   @override
-  _RandomWordsState123 createState() => _RandomWordsState123();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];                
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        final int index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );     
+  }
+}
 
 //lab 1
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+  Widget build(BuildContext context) {   
+     return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    ); 
   }
 }
