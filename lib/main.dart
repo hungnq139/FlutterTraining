60b0243362e06ecc8d21e01b32fc3a1a402c5653
quang-hung.nguyen <quang-hung.nguyen@quant-edge.com>
2021-12-02 09:33:04 +0700

    init project

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class _RandomWordsState123 extends State<RandomWords> {
+  @override                                  
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();      // NEW
+    return Text(wordPair.asPascalCase);      // NEW
+  }                                         
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState123 createState() => _RandomWordsState123();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
